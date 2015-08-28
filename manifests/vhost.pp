ea5d8c33be3a1f243e8d43bdc694359b0a4eceac
Carmela Rubinos <carmela.rubinos@gmail.com>
2015-08-28 10:01:54 +0200

    Use a more recognizable name for vhost
    servername is parameterized and its value defaulted to
    'midonet-manager'. It's assumed that the user will have more than 1
    virtualhost.
    
    If using the default value (midonet-manager), a line should be added to
    /etc/hosts in order to be able to reach it. Otherwise, directly specify
    an IP address or reachable domain.
    
    Change-Id: I0d66a3bf49189eae97181dfe5a34d7d56b92dcdd

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -4,26 +4,28 @@ class midonet_mem::vhost {
   include ::apache
   include ::apache::mod::headers
 
+  $servername = $midonet::mem::params::servername
   $docroot = $midonet_mem::params::mem_install_path
   $mem_package = $midonet_mem::params::mem_package
   $proxy_pass = [
     { 'path' => "/$midonet_mem::params::api_namespace",
-      'url'  => "$midonet_mem::params::api_host"
-    }
+      'url'  => "$midonet_mem::params::api_host",
+    },
+  ]
+  $directories = [
+    { 'path'  => '/var/www/html',
+      'allow' => 'from all',
+    },
   ]
 
   apache::vhost { 'midonet-mem':
     port            => '80',
-    servername      => "http://$::ipaddress",
+    servername      => $servername,
     docroot         => '/var/www/html',
     proxy_pass      => $proxy_pass,
     redirect_source => '/midonet-manager',
     redirect_dest   => $docroot,
-    directories     => [
-      { 'path'      => '/var/www/html',
-        'allow'     => 'from all',
-      },
-    ],
+    directories     => $directories,
     require         => Package["$mem_package"],
   }
 
