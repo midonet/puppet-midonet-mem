d1ebf7451210d5fdc13a2e0a2edda45e803b95f8
Carmela Rubinos <carmela.rubinos@gmail.com>
2015-09-17 10:35:10 +0200

    Parameterized midonet_mem::vhost a bit more
    This patch adds new params for midonet_mem::vhost, as apache_port and
    servername
    
    Change-Id: I62213758a52e3061316ccfb4b04f66463ae79afb

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -1,34 +1,31 @@
 class midonet_mem::vhost (
-  $servername    = $midonet_mem::params::servername,
-  $docroot       = $midonet_mem::params::docroot,
-  $mem_package   = $midonet_mem::params::mem_package,
-  $api_namespace = $midonet_mem::params::api_namespace,
-  $api_host      = $midonet_mem::params::api_host,
-  $apache_port   = $midonet_mem::params::apache_port,
+  $apache_port = $midonet_mem::params::apache_port,
+  $servername  = $midonet_mem::params::servername,
+  $docroot     = $midonet_mem::params::docroot,
+  $mem_package = $midonet_mem::params::mem_package,
+  $mem_path    = $midonet_mem::params::mem_install_path,
+  $proxy_pass = [
+    { 'path' => "/$midonet_mem::params::api_namespace",
+      'url'  => "$midonet_mem::params::api_host",
+    },
+  ],
 ) inherits midonet_mem::params {
 
   include ::apache
   include ::apache::mod::headers
 
-  $proxy_pass = [
-    { 'path' => "/$api_namespace",
-      'url'  => "$api_host",
-    },
-  ]
-  $directories = [
-    { 'path'  => '/var/www/html',
-      'allow' => 'from all',
-    },
-  ]
-
   apache::vhost { 'midonet-mem':
     port            => $apache_port,
     servername      => $servername,
-    docroot         => '/var/www/html',
+    docroot         => $docroot,
     proxy_pass      => $proxy_pass,
     redirect_source => '/midonet-manager',
     redirect_dest   => $docroot,
-    directories     => $directories,
+    directories     => [
+      { 'path'  => '/var/www/html',
+        'allow' => 'from all',
+      },
+    ],
     require         => Package["$mem_package"],
   }
 
